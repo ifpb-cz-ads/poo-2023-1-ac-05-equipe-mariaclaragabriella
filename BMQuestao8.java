8e74762546e71fb0098f10a138262bfe3032a1a3
clara <ramalho.maria@academico.ifpb.edu.br>
2023-04-10 16:40:43 -0300

    Adiciona questões de 6 até 10 do livro Batista & Moraes e também adiciona questões 5 e 6 do anexo

---
+++ BMQuestao8.java
@@ -0,0 +1,10 @@
+public class BMQuestao8 {
+    public static void main(String[] args){
+        int numero = 3;
+        for(int i=1; i<=100; i++){
+            if(i%numero==0){
+                System.out.print(i + " - ");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-
+### Questão 6. 
+O programa da class Printing imprime na tela 10 linhas, em que cada linha tem 5 caracteres do tipo @. O resultado no terminal é o seguinte:
+'''
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+@@@@@
+'''
